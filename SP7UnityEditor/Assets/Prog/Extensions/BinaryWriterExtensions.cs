bfce7c7905a9ed15e317ce826e3f1294b06e1765
AxelSavage <axelsavage@gmail.com>
2021-01-13 17:12:07 +0100

    Added first scripts for Exporting, some extension classes and Prog-Folder Structore in Unity

---
+++ SP7UnityEditor/Assets/Prog/Extensions/BinaryWriterExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class BinaryWriterExtensions
+{
+    /// <summary>  
+    /// <list type="bullet">Max: 16 Chars</list>
+    /// </summary>
+    public static void Write(this BinaryWriter aBinWriter, string aData)
+    {
+        int size = Mathf.Min(16, aData.ToCharArray().Length);
+        aBinWriter.Write(size);
+        aBinWriter.Write(aData.ToCharArray(0, size));
+    }
+
+    /// <summary>  
+    /// <list type="bullet">Writes Max: 16 Chars of Each String found in the List</list>
+    /// </summary>
+    public static void Write(this BinaryWriter aBinWriter, List<string> someData)
+    {
+        foreach (var text in someData)
+            Write(aBinWriter, text);
+    }
+
+    /// <summary>  
+    /// <list type="bullet">X</list>
+    /// <list type="bullet">Y</list>
+    /// </summary>
+    public static void Write(this BinaryWriter aBinWriter, Vector2 aData)
+    {
+        aBinWriter.Write(aData.x);
+        aBinWriter.Write(aData.y);
+    }
+
+    /// <summary>  
+    /// <list type="bullet">X</list>
+    /// <list type="bullet">Y</list>
+    /// <list type="bullet">Z</list>
+    /// </summary>
+    public static void Write(this BinaryWriter aBinWriter, Vector3 aData)
+    {
+        aBinWriter.Write(aData.x);
+        aBinWriter.Write(aData.y);
+        aBinWriter.Write(aData.z);
+    }
+
+    /// <summary>  
+    /// <list type="bullet">Position</list>
+    /// <list type="bullet">Rotation Euler Angles</list>
+    /// <list type="bullet">Local Scale</list>
+    /// </summary>
+    public static void Write(this BinaryWriter aBinWriter, Transform aData)
+    {
+        aBinWriter.Write(aData.position);
+        aBinWriter.Write(aData.rotation.eulerAngles);
+        aBinWriter.Write(aData.localScale);
+    }
+}
