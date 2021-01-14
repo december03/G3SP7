85444f11bb91feaf3fe2e818730ab71133b3eb01
AxelSavage <axelsavage@gmail.com>
2021-01-14 14:41:19 +0100

    Added First iteration of some SceneManagement to make life easier when using multiple scenes to build levels

---
+++ SP7UnityEditor/Assets/Prog/Editor/LevelCollectionEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelCollection))]
+public class LevelCollectionEditor : Editor
+{
+    Object obj = null;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        obj = EditorGUILayout.ObjectField("Scene Asset", obj, typeof(SceneAsset), false);
+        if (GUILayout.Button("Add Scene"))
+        {
+            LevelCollection levelCollection = (LevelCollection)target;
+            SceneReference sceneReference = new SceneReference();
+            sceneReference.ScenePath = AssetDatabase.GetAssetPath(obj.GetInstanceID());
+            levelCollection.myScenes.Add(sceneReference);          
+        }
+    }
+}
---
+++ SP7UnityEditor/Assets/Prog/ScriptableObjectScripts/LevelCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="LD/Level Collection")]
+public class LevelCollection : ScriptableObject
+{
+    public List<SceneReference> myScenes;
+}
